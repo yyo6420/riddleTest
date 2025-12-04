89e66ed6710f1d4cbae69815bf0be17061bd7bd2
yyo6420 <yyo6420@gmail.com>
2025-12-04 10:23:50 +0200

    allRiddles

---
+++ allRilddles.js
@@ -0,0 +1,12 @@
+import { riddleOne } from "./riddle1.js";
+import { riddleTwo } from "./riddle2.js";
+import { riddleThree } from "./riddle3.js";
+import { riddleFour } from "./riddle4.js";
+import { riddleFive } from "./riddle5.js";
+export const riddles = [
+  riddleOne,
+  riddleTwo,
+  riddleThree,
+  riddleFour,
+  riddleFive,
+];
