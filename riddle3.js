290811e9a2ca476e509efe1f093072d1c4128dc2
yyo6420 <yyo6420@gmail.com>
2025-12-04 09:57:48 +0200

    riddleThree

---
+++ riddle3.js
@@ -0,0 +1,7 @@
+export const riddleThree = {
+  id: "3",
+  nmae: "Inventions",
+  taskDescription: "Which Which Which invention is an israeli invention?",
+  choices: ["Washing machine", "USB flash drive", "Crane", "Satellite"],
+  correctAnswer: "USB flash drive",
+};
