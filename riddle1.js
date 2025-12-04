cd55367dfb2ec0121556d3ba0e0f4b99f5483c6d
yyo6420 <yyo6420@gmail.com>
2025-12-04 09:46:44 +0200

    riddleTwo

--- riddle1.js
+++ riddle1.js
@@ -2,5 +2,6 @@ export const riddleOne = {
   id: "1",
   nmae: "Eazy Math",
   taskDescription: "What is 7 + 9?",
+  choices: ["2", "15", "16", "21"],
   correctAnswer: "16",
 };
---
+++ riddle2.js
@@ -0,0 +1,7 @@
+export const riddleTwo = {
+  id: "2",
+  nmae: "Sport game",
+  taskDescription: "Which Which sport doesn't use a ball?",
+  choices: ["Tennis", "football", "Snooker", "Hockey"],
+  correctAnswer: "Hockey",
+};
