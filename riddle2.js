aee288c392b9635bddfa57c7efcea4b1a1a19b28
yyo6420 <yyo6420@gmail.com>
2025-12-04 10:14:44 +0200

    riddleFive

--- riddle2.js
+++ riddle2.js
@@ -1,6 +1,6 @@
 export const riddleTwo = {
   id: "2",
-  nmae: "Sport game",
+  nmae: "Sport Game",
   taskDescription: "Which Which sport doesn't use a ball?",
   choices: ["Tennis", "football", "Snooker", "Hockey"],
   correctAnswer: "4",
---
+++ riddle5.js
@@ -0,0 +1,7 @@
+export const riddleFive = {
+  id: "5",
+  nmae: "Football Clubs",
+  taskDescription: "Which football club does not play in La Liga?",
+  choices: ["Girona", "Barcelona", "Getafe", "Chelsea?"],
+  correctAnswer: "4",
+};
