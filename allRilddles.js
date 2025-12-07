55efed7eb2d66d93c44b2c424feee017dd92e9a3
yyo6420 <yyo6420@gmail.com>
2025-12-07 10:34:46 +0200

    riddleSix

--- allRilddles.js
+++ allRilddles.js
@@ -3,10 +3,12 @@ import { riddleTwo } from "./riddle2.js";
 import { riddleThree } from "./riddle3.js";
 import { riddleFour } from "./riddle4.js";
 import { riddleFive } from "./riddle5.js";
+import { riddleSix } from "./riddle6.js";
 export const riddles = [
   riddleOne,
   riddleTwo,
   riddleThree,
   riddleFour,
   riddleFive,
+  riddleSix
 ];
---
+++ riddle6.js
@@ -0,0 +1,7 @@
+export const riddleSix = {
+  id: "6",
+  nmae: "Capital Cities",
+  taskDescription: "What is the capital city of England?",
+  choices: ["liverpool", "London", "Manchester", "Birmingham"],
+  correctAnswer: "2",
+};
