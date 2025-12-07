56046f600da3dfd7e7805669496591e85cc852f5
yyo6420 <yyo6420@gmail.com>
2025-12-07 13:13:28 +0200

    function measureSolveTime

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import input from "analiza-sync";
 import { riddles } from "./allRilddles.js";
-import { addSloveTime } from "./utills.js";
+import { addSloveTime, gameStatistics, measureSolveTime } from "./utills.js";
 import { askRidlle } from "./utills.js";
 import { showStatus } from "./utills.js";
 import { createPlayer } from "./player.js";
@@ -9,15 +9,18 @@ function gameFlow() {
   const askForName = input(
     "Hello and welcome to our Trivia Riddles Game\nlet's get started\nwhat is your mane? "
   );
-  console.log("To answer a question, you must choose a number from 1 to 4")
+  console.log("To answer a question, you must choose a number from 1 to 4");
   const player1 = createPlayer(askForName);
-  addSloveTime(
-    player1,
-    riddles.forEach((riddle) => {
-      askRidlle(riddle);
-    })
+  gameStatistics["timer"] += measureSolveTime(() =>
+    addSloveTime(
+      player1,
+      riddles.forEach((riddle) => {
+        askRidlle(riddle);
+      })
+    )
   );
-  showStatus()
+
+  showStatus();
 }
 
-gameFlow()
+gameFlow();
--- utills.js
+++ utills.js
@@ -12,22 +12,16 @@ export function addSloveTime(player, seconds) {
 }
 
 export function askRidlle(ridlle) {
-  const statTimer = Date.now();
   const answer = input(
     `${ridlle["taskDescription"]} \n${ridlle["choices"]} \n`
   );
   if (answer === ridlle["correctAnswer"]) {
     console.log("Correct :)");
     gameStatistics["correctAnswers"] += 1;
-    const stopTimer = Math.floor((Date.now() - statTimer) / 1000);
-    gameStatistics["timer"] += stopTimer;
   } else {
     console.log("Sorry, that's incorrect :(");
     gameStatistics["wrongAnswers"] += 1;
-    const stopTimer = Math.floor((Date.now() - statTimer) / 1000);
-    gameStatistics["timer"] += stopTimer;
   }
-  return statTimer;
 }
 
 export function showStatus() {
@@ -36,4 +30,11 @@ export function showStatus() {
     (gameStatistics["correctAnswers"] + gameStatistics["wrongAnswers"]);
   gameStatistics["timeAverage"] += average;
   console.log(gameStatistics);
+}
+
+export function measureSolveTime(fn) {
+  const statTimer = Date.now();
+  fn();
+  const stopTimer = Math.floor((Date.now() - statTimer) / 1000);
+  return stopTimer;
 }
